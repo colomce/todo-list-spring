d6797444f7544480f2af9c274490b6b1e2326135
BORBECH <charlieborbs18@gmail.com>
2020-10-29 15:03:04 +0800

    [ToDoList][Charlie-Cedric]  test  : Create test for creating todo

--- src/test/java/com/oocl/todoapp/Integration/TodoIntegrationTest.java
+++ src/test/java/com/oocl/todoapp/Integration/TodoIntegrationTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -34,6 +35,19 @@ public class TodoIntegrationTest {
                 .andExpect(jsonPath("$[0].id").isNumber())
                 .andExpect(jsonPath("$[0].text").value("Code"))
                 .andExpect(jsonPath("$[0].done").value(false));
+    }
+
+    @Test
+    public void should_create_todo_when_create_given_todo() throws Exception {
+        //given
+        Todo todo = new Todo(1,"Code", false);
+
+        //when then
+        mockMvc.perform(post("/api/todos"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.text").value("Code"))
+                .andExpect(jsonPath("$.done").value(false));
     }
 
 }
