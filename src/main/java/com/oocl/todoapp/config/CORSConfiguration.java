91d9c35a2a6b871700c709e490c205187fcf3cae
Cedric Don Coloma <colomce@oocl.com>
2020-10-29 16:15:22 +0800

    [SpringTodo][Cedric-Charlie] feature: Changed Cors pattern

--- src/main/java/com/oocl/todoapp/config/CORSConfiguration.java
+++ src/main/java/com/oocl/todoapp/config/CORSConfiguration.java
@@ -12,7 +12,7 @@ public class CORSConfiguration {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/todos**")
+                registry.addMapping("/api/**")
                         .allowedOrigins("http://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
