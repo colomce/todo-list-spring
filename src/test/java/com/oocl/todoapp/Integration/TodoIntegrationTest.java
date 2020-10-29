f990a660a5848f92dd8b37851d7004f94d0457d0
BORBECH <charlieborbs18@gmail.com>
2020-10-29 14:34:48 +0800

    [ToDoList][Charlie-Cedric]  test  : Create integration test to get all todos

---
+++ src/test/java/com/oocl/todoapp/Integration/TodoIntegrationTest.java
@@ -0,0 +1,39 @@
+package com.oocl.todoapp.Integration;
+
+import com.oocl.todoapp.models.Todo;
+import com.oocl.todoapp.repository.TodoRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TodoIntegrationTest {
+
+    @Autowired
+    private TodoRepository todoRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void should_return_all_todos_when_getAll_given_1_todo() throws Exception {
+        //given
+        Todo todo = new Todo(1,"Code", false);
+        todoRepository.save(todo);
+
+        //when then
+        mockMvc.perform(get("/api/todos"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").isNumber())
+                .andExpect(jsonPath("$[0].text").value("Code"))
+                .andExpect(jsonPath("$[0].done").value(false));
+    }
+
+}
