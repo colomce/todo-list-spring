75cf3c969192e0e28ba9c529867446786eaf2dd4
BORBECH <charlieborbs18@gmail.com>
2020-10-29 14:58:21 +0800

    [ToDoList][Charlie-Cedric]  refactor  : Added necessary annotations and default constructor

--- src/main/java/com/oocl/todoapp/models/Todo.java
+++ src/main/java/com/oocl/todoapp/models/Todo.java
@@ -1,9 +1,20 @@
 package com.oocl.todoapp.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String text;
     private Boolean done;
+
+    public Todo() {
+    }
 
     public Todo(Integer id, String text, Boolean done) {
         this.id = id;
