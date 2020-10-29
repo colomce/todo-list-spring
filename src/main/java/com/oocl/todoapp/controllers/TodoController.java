a5d7caf7ae0a05492fcd8d0d496d009d187f508c
BORBECH <charlieborbs18@gmail.com>
2020-10-29 15:18:16 +0800

    [ToDoList][Charlie-Cedric] feature : Added TodoController for creating todo

--- src/main/java/com/oocl/todoapp/controllers/TodoController.java
+++ src/main/java/com/oocl/todoapp/controllers/TodoController.java
@@ -1,11 +1,12 @@
 package com.oocl.todoapp.controllers;
 
+import com.oocl.todoapp.dto.TodoRequest;
 import com.oocl.todoapp.dto.TodoResponse;
 import com.oocl.todoapp.mapper.TodoMapper;
+import com.oocl.todoapp.models.Todo;
 import com.oocl.todoapp.services.TodoService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,6 +25,14 @@ public class TodoController {
     @GetMapping
     private List<TodoResponse> getAll() {
         return todoMapper.toResponseList(todoService.getAll());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    private TodoResponse create(@RequestBody TodoRequest todoRequest){
+        Todo todo = todoMapper.toEntity(todoRequest);
+        Todo createdTodo = todoService.create(todo);
+        return todoMapper.toResponse(createdTodo);
     }
 
 
